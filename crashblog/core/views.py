3664b952739a2880e0a4e705d3c4812308f0ad95
ruthjelimo <ruthjmimo@gmail.com>
2023-06-12 11:27:12 +0300

    update the file

---
+++ crashblog/core/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from blog.models import Post
+# from django.http.response import HttpResponse
+from django.http import HttpResponse
+# Create your views here.
+def frontpage(request):
+    posts = Post.objects.filter(status = Post.ACTIVE)
+    return render(request, 'core/frontpage.html',{'posts': posts})
+
+def about(request):
+    return render(request, 'core/about.html')
+
+def robots_txt(request):
+    text = [
+        "User-Agent: *",
+        "Disallow: /admin/",
+    ]
+    return HttpResponse("\n".join(text), content_type="text/plain")
